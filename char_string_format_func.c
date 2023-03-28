7afbb771ba49fc75ae3d836330660b3072fcdcd0
Allison Ogechukwu <44110875+AllisonOge@users.noreply.github.com>
2023-03-28 14:56:18 +0000

    added 3 files to contain format handling functions

---
+++ char_string_format_func.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * print_char - prints a character
+ * @args: list of arguments
+ * @buffer: buffer array to handle print
+ *
+ * Return: number of chars printed except the null byte
+ */
+int print_char(va_list args, char buffer[])
+{
+	va_list args_copy;
+	char c;
+
+	UNUSED(buffer);
+	va_copy(args_copy, args);
+	c = va_arg(args_copy, int);
+
+	return (_putchar(c));
+}
+
+/**
+ * print_string - prints a string
+ * @args: list of arguments
+ * @buffer: buffer array to handle print
+ *
+ * Return: number of chars printed except the null byte
+ */
+int print_string(va_list args, char buffer[])
+{
+	va_list args_copy;
+	int i, len = 0;
+	char *s;
+
+	UNUSED(buffer);
+	va_copy(args_copy, args);
+	s = va_arg(args_copy, char *);
+
+	/* find the length of string */
+	while (s[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+		_putchar(s[i]);
+
+	return (len);
+}
+
+/**
+ * print_percent - print percent character
+ * @args: list of arguments
+ * @buffer: buffer array to handle print
+ *
+ * Return: number of chars printed except null byte
+ */
+int print_percent(va_list args, char buffer[])
+{
+	UNUSED(args);
+	UNUSED(buffer);
+	return (_putchar('%'));
+}
