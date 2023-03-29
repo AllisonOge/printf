7d66857966ea77d3da2ce21d735d0355e52fc6df
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-03-30 00:19:50 +0100

    refactorint to allow more test cases

--- test/test_printf.cc
+++ test/test_printf.cc
@@ -1,30 +1,36 @@
 #include "gtest/gtest.h"
-extern "C" {
-    #include "main.h"
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <string.h>
+extern "C"
+{
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 }
 
-
-// Test case that the test works
-TEST(TestPrintf, TestWorks) {
-    EXPECT_EQ(5, 5);
-}
-
-// Test case to assert the output with conversion specifiers c s and %
-TEST(TestPrintf, TestCharStringPercent) {
+/**
+ * testPrintf - assert the output through the stdout
+ * @printFunc: function that prints values
+ */
+void testPrintf(int (*printFunc)(const char *, ...), const char *str, ...)
+{
     // redirect output to a file
-    FILE* tmp = tmpfile();
+    FILE *tmp = tmpfile();
     if (tmp == NULL)
         FAIL() << "Unable to create temporary file";
-    
+
     fflush(stdout);
     int old_stdout = dup(fileno(stdout));
     dup2(fileno(tmp), fileno(stdout));
 
+    // get the expected value
+    char expected[BUFF_SIZE];
+
     // Call function that prints value
-    _printf("Hello World!\n");
+    va_list args;
+    va_start(args, str);
+    sprintf(expected, str, args);
+    printFunc(str, args);
+    va_end(args);
 
     // restore the orignal output stream
     fflush(stdout);
@@ -32,12 +38,27 @@ TEST(TestPrintf, TestCharStringPercent) {
     rewind(tmp);
 
     // read the content of the file
-    char buffer[BUFF_SIZE];
-    memset(buffer, 0, sizeof(buffer));
-    fread(buffer, sizeof(char), sizeof(buffer) - 1, tmp);
+    char actual[BUFF_SIZE];
+    memset(actual, 0, sizeof(actual));
+    fread(actual, sizeof(char), sizeof(actual) - 1, tmp);
     fclose(tmp);
 
     // check the contents of the file
-    EXPECT_STREQ("Hello World!\n", buffer);
+    ASSERT_STREQ(expected, actual);
 }
 
+/***** Test case to assert the output with conversion specifiers c s and % ***/
+
+// Test case to assert funtion prints format
+TEST(TestPrintf, TestString)
+{
+    const char *format = "Hello World!\n";
+    testPrintf(&_printf, format);
+}
+
+// Test case to assert function prints with conversion specifier c
+TEST(TestPrintf, TestChar)
+{
+    const char *format = "Character:[%c]\n";
+    testPrintf(&_printf, format, 'H');
+}
